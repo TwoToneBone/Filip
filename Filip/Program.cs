e0fb82fb7c97c987b6ed9ce8cf6d7bd8f04d381d
Erik <erik.ra@gmail.com>
2017-05-22 16:15:03 +0200

    Add project files.

---
+++ Filip/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] inputs = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(new string(inputs[0].Reverse().ToArray()));
+            int b = int.Parse(new string(inputs[1].Reverse().ToArray()));
+
+            if (a > b)
+            {
+                Console.WriteLine(a);
+            }
+            else
+            {
+                Console.WriteLine(b);
+            }
+        }
+    }
+}
